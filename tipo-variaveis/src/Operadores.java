665b4c41cc8ab72863a4b0358b876183a5a33c04
wilsxnjr <99342358+wilsxnjr@users.noreply.github.com>
2025-03-20 09:13:33 +0200

    Operador ternário

--- tipo-variaveis/src/Operadores.java
+++ tipo-variaveis/src/Operadores.java
@@ -23,5 +23,13 @@ public class Operadores {
 
         System.out.println(concatenacao);
 
+        //Operador ternario
+
+        int a = 5;
+        int b = 6;
+
+        String resultado = (a==b) ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
     }
 }
--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args)  {
         double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
         
     }
 }
