6c5db31f79f5c3c4a63445566b7371b45f35385d
wilsxnjr <99342358+wilsxnjr@users.noreply.github.com>
2025-03-20 23:43:12 +0200

    Java doc e comentários

---
+++ java-doc/src/Calculadora.java
@@ -0,0 +1,19 @@
+/**
+ * @author Wilson Junior
+ * @version 1.0
+ * @since 20/03/2025
+ */
+
+public class Calculadora {
+
+    /**
+     * Este método é para somar dois números
+     * @param numeroUm
+     * @param numeroDois
+     * @return o resultado deste método é a soma de dois números
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+
+}
---
+++ java-doc/src/Comentario.java
@@ -0,0 +1,9 @@
+public class Comentario {
+    public static void main(String[] args) {
+        // Comentário em uma linha
+        /*
+         * Comentário mais detalhado
+         * TESTE
+         */
+    }
+}
